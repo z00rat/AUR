fb6aa77573229998949e6c2cd4fb1172a7d4dffc
zoorat <78788887+z00rat@users.noreply.github.com>
2021-03-17 10:22:56 +0600

    adding backup.sh

---
+++ backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+msg2 "copying eveything from repos to pkgs"
+cp -r repos/* pkgs
+
+for gitfile in $(tree -fia pkgs | grep -E ".git$")
+do
+    msg2 "deleting '$gitfile'"
+    rm $gitfile
+done
+
+msg2 "status:"
+git status
+
+msg2 "file tree:"
+tree -fia pkgs
+
+msg2 "commting changes in 10s (CTRL+C to exit)"
+sleep 10
+git commit -a -m "$(date)"
+git push
+
+msg2 "done."
