ca2b0b096e1f7dad69bb178214fa5c51cfb989fe
zoorat <78788887+z00rat@users.noreply.github.com>
2021-03-17 07:45:02 +0600

    adding repo adding script

---
+++ add.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for pkgname in $@ 
+do
+   msg2 "cloning AUR repo of '$pkgname'"
+
+   cd AUR
+   git submodule add --force -b master ssh://aur@aur.archlinux.org/$pkgname.git
+   git commit -m "adding $pkgname"
+   cd ..
+
+done
+
+msg2 "done cloning"
