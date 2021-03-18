9ea0ce87472b3a9cf6aa0274698d2cc6d2001cc5
zoorat <78788887+z00rat@users.noreply.github.com>
2021-03-18 11:00:56 +0600

    add nwely installed aur pkgs

---
+++ addnew.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+msg2 "list of packages that are already tracked "
+/usr/bin/ls repos > alreadytracked.txt
+msg2 "number of packags that are $(cat alreadytracked.txt | wc -l) tracked"
+
+msg2 "new packages "
+pacman -Qqm > alreadyinstalled.txt
+msg2 "number of packages that are $(cat alreadyinstalled.txt | wc -l) installed"
+
+./add.sh $(diff alreadyinstalled.txt alreadytracked.txt | grep "<" | cut -c 3-)
+
+trash alreadyinstalled.txt alreadytracked.txt
+
+msg2 "done."
