1cf459cd4a3394437fa76475ac91fbe1564aa560
zoorat <78788887+z00rat@users.noreply.github.com>
2021-09-21 12:50:44 +0600

    no more addnew.sh from now only status.sh
    Signed-off-by: zoorat <78788887+z00rat@users.noreply.github.com>

---
+++ status.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+/usr/bin/ls repos | sort >alreadytracked.txt
+msg2 "number of packags that are $(wc <alreadytracked.txt -l) tracked"
+
+pacman -Qqm | sort >alreadyinstalled.txt
+msg2 "number of packages that are $(wc <alreadyinstalled.txt -l) installed"
+
+msg2 "packages that are not tracked but installed in the system..."
+echo "$(diff alreadyinstalled.txt alreadytracked.txt | grep "<" | cut -c 3-)"
+
+msg2 "packages that are tracked but not installed in the system..."
+echo "$(diff alreadyinstalled.txt alreadytracked.txt | grep ">" | cut -c 3-)"
+
+trash alreadyinstalled.txt alreadytracked.txt
+msg2 "done."
