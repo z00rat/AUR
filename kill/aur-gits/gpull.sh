de3a0118adc0874d7d7b1fec78885afc80df7e82
zoorat <78788887+z00rat@users.noreply.github.com>
2021-11-25 09:53:03 +0600

    Thu Nov 25 09:52:54 AM +06 2021
    Signed-off-by: zoorat <78788887+z00rat@users.noreply.github.com>

--- kill/aur-gits/gpull.sh
+++ kill/aur-gits/gpull.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-< "$1".names xargs -n 1 -I XXXXXX sh -c 'git clone https://aur.archlinux.org/XXXXXX.git ./packages/XXXXXX && sleep 2'
+< "$1".names xargs -I XXXXXX sh -c 'git clone https://aur.archlinux.org/XXXXXX.git ./packages/XXXXXX && sleep 2'
