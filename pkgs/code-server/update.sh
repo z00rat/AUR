c264ee0c57c82c6685d56480f484bf68b3933a12
zoorat <78788887+z00rat@users.noreply.github.com>
2022-05-07 06:23:05 +0000

    Sat May  7 06:22:20 AM UTC 2022
    Signed-off-by: zoorat <78788887+z00rat@users.noreply.github.com>

--- pkgs/code-server/update.sh
+++ pkgs/code-server/update.sh
@@ -41,6 +41,11 @@ main() {
   makepkg --printsrcinfo > .SRCINFO
 
   echo "All updated!"
+  echo "Committing and pushing to GitHub"
+  git add .
+  git commit -m "chore: updating version $CODE_SERVER_VERSION_TO_UPDATE"
+  git push
+  echo "Action requried: make sure to push to aur: git push aur"
 }
 
 main "$@"
