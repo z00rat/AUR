1d2505b6d015f3c929acf72fe174ff1ab892ddef
zoorat <78788887+z00rat@users.noreply.github.com>
2023-06-24 07:04:39 +0000

    modding a bit...
    Signed-off-by: zoorat <78788887+z00rat@users.noreply.github.com>

--- backup.sh
+++ backup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-msg2 "copying eveything from repos to pkgs"
+msg2 "copying eveything from ./repos to ./pkgs"
 cp -vr repos/* pkgs
 
+msg2 "deleting .git files"
 for gitfile in $(tree -fia pkgs | grep -E "\.git$"); do
-    msg2 "deleting '$gitfile'"
-    rm "$gitfile"
+    rm -v "$gitfile"
 done
 
+msg2 "adding ./pkgs"
 git add pkgs
 
-msg2 "status:"
+msg2 "git status:"
 git status
 
 # msg2 "file tree:"
@@ -20,6 +21,7 @@ sleep 10
 
 msg2 "commit msg: \"$(date)\""
 git commit -a -s -m "$(date)"
+msg2 "pushing new commit"
 git push
 
 msg2 "done."
